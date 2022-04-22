0e7507c3729ee208a925d94034eaefa39ebc9548
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2022-04-22 23:09:34 +0530

    be changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(ENVIRONMENT.PORT);
+  await app.listen(ENVIRONMENT.PORT || 8080);
 }
 bootstrap();
